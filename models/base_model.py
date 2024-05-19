ed186d500a5b938aaac0bfc934f8fb8ae75f1d43
Ahmed7313 <your-email@example.com>
2024-05-19 01:49:08 -0700

    task 2 modifying the baseModel class and write the dic test

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,18 @@ from datetime import datetime
 class BaseModel:
     """Base class for all models in the AirBnB clone project."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Return a string representation of the instance."""
