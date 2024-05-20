0ee4edb3825529d81f67d6ff7198ff232aa07492
Ahmed7313 <your-email@example.com>
2024-05-19 20:42:41 -0700

    q5 fix

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,12 @@ class BaseModel:
     BaseModel class for AirBnB clone project
     """
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instance of BaseModel
-        """
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.fromisoformat(value)
-                setattr(self, key, value)
+                if key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -27,16 +25,10 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """
-        Updates the public instance attribute updated_at
-        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values of __dict__
-        """
         dict_repr = self.__dict__.copy()
         dict_repr['__class__'] = self.__class__.__name__
         dict_repr['created_at'] = self.created_at.isoformat()
@@ -44,8 +36,5 @@ class BaseModel:
         return dict_repr
 
     def __str__(self):
-        """
-        Returns a string representation of the instance
-        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,42 +7,28 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file and deserializes JSON file to instances
-    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns the dictionary __objects
-        """
         return self.__objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with key <obj class name>.id
-        """
         key = "{}.{}".format(type(obj).__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes __objects to the JSON file
-        """
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """
-        Deserializes the JSON file to __objects
-        """
         try:
             with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
                 for key, value in obj_dict.items():
                     cls_name = value['__class__']
-                    self.__objects[key] = globals()[cls_name](**value)
+                    cls = globals()[cls_name]
+                    self.__objects[key] = cls(**value)
         except FileNotFoundError:
             pass
